0bf0f72f10383809ab7f96a51929ec5e1c621902
Danil Kostromin <danil.kostromin@icloud.com>
2024-11-28 04:28:39 +0300

    refactor(report): [table] allow scroll only when ctrl or meta key is pressed
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/bublik/features/run-report/src/lib/run-report-test/run-report-test.component.tsx
+++ libs/bublik/features/run-report/src/lib/run-report-test/run-report-test.component.tsx
@@ -9,6 +9,7 @@ import {
 } from '@radix-ui/react-hover-card';
 
 import { ArgsValBlock, RecordBlock } from '@/shared/types';
+import { usePlatformSpecificCtrl } from '@/shared/hooks';
 import {
 	CardHeader,
 	Icon,
@@ -181,6 +182,9 @@ function MeasurementBlock(props: RunReportEntityBlockProps) {
 				?.removeEventListener('scroll', handleScroll);
 	}, [offset]);
 
+	const tableScrollRef = useRef<HTMLDivElement>(null);
+	const isPressed = usePlatformSpecificCtrl();
+
 	return (
 		<div className="flex flex-col pl-1">
 			<div className="flex flex-col max-h-[412px]" id={encodeURIComponent(id)}>
@@ -229,7 +233,13 @@ function MeasurementBlock(props: RunReportEntityBlockProps) {
 								enableChartView && chart && 'border-l border-border-primary'
 							)}
 						>
-							<div className="flex-1 overflow-auto">
+							<div
+								className={cn(
+									'flex-1',
+									isPressed ? 'overflow-y-auto' : 'overflow-y-hidden'
+								)}
+								ref={tableScrollRef}
+							>
 								<RunReportTable table={table} />
 							</div>
 						</div>
