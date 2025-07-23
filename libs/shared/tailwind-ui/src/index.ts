51b7478e4fe9e480fa1b547e4ce1124505ed1be3
Danil Kostromin <danil.kostromin@icloud.com>
2025-07-23 04:44:59 +0300

    feat(ui): [split-button] add new component for split-button
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- libs/shared/tailwind-ui/src/index.ts
+++ libs/shared/tailwind-ui/src/index.ts
@@ -69,3 +69,4 @@ export * from './lib/tabs';
 export * from './lib/instruction-dialog';
 export * from './lib/data-table-faceted-filter';
 export * from './lib/run-running';
+export * from './lib/split-button';
